01f7323c2331f219ed5cc8f34cea7dcc4ef8ec47
Sajid18May <sajid1852002@gmail.com>
2025-10-12 22:17:27 +0530

    added APIResponse

--- auth-service/src/main/java/com/auth/service/JwtService.java
+++ auth-service/src/main/java/com/auth/service/JwtService.java
@@ -1,5 +1,9 @@
 package com.auth.service;
 
-public class JwtService {
+import org.springframework.stereotype.Service;
 
+@Service
+public class JwtService {
+	private static final String SECRET_KEY="MySecretKey";
+	private static final long EXPIRATION_TIME=86400000;
 }
