7aefd6043d51aa25419ee1d0463253ea103c1b9a
Sajid18May <sajid1852002@gmail.com>
2025-10-13 23:31:52 +0530

    login and register method added methods

--- auth-service/src/main/java/com/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/auth/controller/AuthController.java
@@ -1,8 +1,29 @@
 package com.auth.controller;
 
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.auth.dto.APIResponse;
+import com.auth.dto.LoginDto;
+import com.auth.dto.UserDto;
+
 @RestController
+@RequestMapping("/auth")
 public class AuthController {
+	
+	@PostMapping("/login")
+	public ResponseEntity<APIResponse<String>> userLogin(LoginDto dto) {
+		APIResponse<String> response = new APIResponse<>();
+		return new ResponseEntity<APIResponse<String>>(response, HttpStatusCode.valueOf(response.getStatus()));
+	}
+	
+	@PostMapping("/register")
+	public ResponseEntity<APIResponse<String>> userRestration(UserDto dto) {
+		APIResponse<String> response = new APIResponse<>();
+		return new ResponseEntity<APIResponse<String>>(response, HttpStatusCode.valueOf(response.getStatus()));
+	}
 
 }
